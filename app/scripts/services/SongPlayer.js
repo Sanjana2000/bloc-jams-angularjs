2a4735c8d03a17473036068e4d0ebce3157a9b16
Sanjana2000 <sanjana100@gmail.com>
2018-09-11 16:41:04 -0700

    Error fixed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,25 +1,4 @@
 (function() {
-<<<<<<< HEAD
-     function SongPlayer() {
-          var SongPlayer = {};
-
-          SongPlayer.play = function(song) {
-                   var currentBuzzObject = new buzz.sound(song.audioUrl, {
-                       formats: ['mp3'],
-                       preload: true
-                   });
-
-                   currentBuzzObject.play();
-               };
-
-          return SongPlayer;
-     }
-
-     angular
-         .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
- })();
-=======
   function SongPlayer() {
     var SongPlayer = {};
 
@@ -78,4 +57,3 @@
     .factory('SongPlayer', SongPlayer);
 
 })();
->>>>>>> CP-Services-Part2
