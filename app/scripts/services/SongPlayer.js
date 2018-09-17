75f8b03b4f7fdb9c7e835f22a9825f875452d26a
Sanjana2000 <sanjana100@gmail.com>
2018-09-17 11:29:11 -0700

    Added missing link of controller-LandingCtrl in index.html

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,66 +1,73 @@
-function() {
-  function SongPlayer() {
-    var SongPlayer = {};
-    var currentSong = null;
-    /*
-     * @desc Buzz object audio file
-     * @type {Object}
-     */
-    var currentBuzzObject = null;
-    /*
-     * @function setSong
-     * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-     * @param {Object} song
-     */
-    var setSong = function(song) {
-      if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        currentSong.playing = null;
-      }
-      currentBuzzObject = new buzz.sound(song.audioUrl, {
-        formats: ['mp3'],
-        preload: true
-      });
-      currentSong = song;
-    };
-
-
-    /*
-     * @function playSong
-     * @desc Plays currently playing song.
-     * @param {Object} song
-     */
-    var playSong = function(song) {
-      if (currentBuzzObject) {
-        currentBuzzObject.play();
-        currentSong.playing = true;
-      };
+(function() {
+    function SongPlayer() {
+      var SongPlayer = {};
+      var currentSong = null;
 
 
 
       /*
-       * @function SongPlayer
-       * @desc sets and plays the current Buzz object
-       * @param song
-       * @type object
+       * @desc Buzz object audio file
+       * @type {Object}
        */
-      SongPlayer.play = function(song) {
-        if (currentSong !== song) {
-          setSong(song);
-          playSong(song);
-        } else if (currentSong === song) {
-          if (currentBuzzObject.isPaused()) {
-            currentBuzzObject.play();
-          }
+      var currentBuzzObject = null;
+
+
+
+      /*
+       * @function setSong
+       * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+       * @param {Object} song
+       */
+      var setSong = function(song) {
+        if (currentBuzzObject) {
+          currentBuzzObject.stop();
+          currentSong.playing = null;
         }
-      };
-      SongPlayer.pause = function(song) {
-        currentBuzzObject.pause();
-        song.playing = false;
-      };
-      return SongPlayer;
-    }
-    angular
-      .module('blocJams')
-      .factory('SongPlayer', SongPlayer);
-  })();
+        currentBuzzObject = new buzz.sound(song.audioUrl, {
+          formats: ['mp3'],
+          preload: true
+        });
+        currentSong = song;
+      /*check };*/
+
+
+      /*
+       * @function playSong
+       * @desc Plays currently playing song.
+       * @param {Object} song
+       */
+      var playSong = function(song) {
+        if (currentBuzzObject) {
+          currentBuzzObject.play();
+          currentSong.playing = true;
+        }
+
+
+
+        /*
+         * @function SongPlayer
+         * @desc sets and plays the current Buzz object
+         * @param song
+         * @type object
+         */
+        SongPlayer.play = function(song) {
+          if (currentSong !== song) {
+            setSong(song);
+            playSong(song);
+          } else if (currentSong === song) {
+            if (currentBuzzObject.isPaused()) {
+              currentBuzzObject.play();
+            }
+          }
+        };
+        SongPlayer.pause = function(song) {
+          currentBuzzObject.pause();
+          song.playing = false;
+        };
+        return SongPlayer;
+/*below correct is } */
+};
+      angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+          })();
