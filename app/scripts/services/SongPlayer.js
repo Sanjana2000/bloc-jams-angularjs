37def2467a96dfaecbd56baf6e42e4adf6ef42a2
Sanjana2000 <sanjana100@gmail.com>
2018-09-20 17:23:29 -0700

    fixed error

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -126,6 +126,20 @@
        }
  };
 
+ SongPlayer.next = function() {
+  var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+  currentSongIndex++;
+
+  if (currentSongIndex >= currentAlbum.songs.length) {
+      currentBuzzObject.stop();
+      SongPlayer.currentSong.playing = null;
+    } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+    }
+};
+
 
 
     return SongPlayer;
